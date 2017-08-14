4f687ef5368f3ae60de4f2142c764ebba00c5b1e
Dave <davekennewell@gmail.com>
2017-08-14 21:39:21 +0400

    first version to work on anuganode

--- woll_ex_01.py
+++ woll_ex_01.py
@@ -1,94 +1,110 @@
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 # IMPORT NECESSARY MODULES
 #----------------------------------------------------------------------------------------------------------------------------------------------------
-print 'About to Start Simulation:- Importing Modules'
+print 'Setting up Simulation:- Importing Modules'
 
 import anuga, numpy, time, os, glob
 from anuga.operators.rate_operators import Polygonal_rate_operator
 from anuga import file_function, Polygon_function, read_polygon, create_mesh_from_regions, Domain
 import anuga.utilities.quantity_setting_functions as qs
 import anuga.utilities.spatialInputUtil as su
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-#  ADD CATCHMENT INFORMATION HERE 
-#----------------------------------------------------------------------------------------------------------------------------------------------------
+import getpass
 
-# CatchmentDictionary = {
-#     'Model/Bdy/Fine.csv':5
-#     }
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-# FILENAMES, MODEL DOMAIN and VARIABLES
-#----------------------------------------------------------------------------------------------------------------------------------------------------
+def run2_woll_ex_01(sim_id):
+    print "run2_woll_ex_01 has fired with sim_id: %s" % sim_id
+    base_dir = os.getcwd() + '/woll_ex_01'
+    print "base_dir is: %s" % base_dir
 
-basename = 'inputs/DEM/dem_existing_01'
-outname = 'outputs/woll_ex_01'
-meshname = 'outputs/woll_ex_01.msh'
+    #print "linux user is: " + getpass.getuser()
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    #  ADD CATCHMENT INFORMATION HERE
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
 
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-# ENTER DOMAIN COORDINATES
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-# W=301300
-# N=6186300
-# E=302000
-# S=6185700
+    # CatchmentDictionary = {
+    #     'Model/Bdy/Fine.csv':5
+    #     }
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    # FILENAMES, MODEL DOMAIN and VARIABLES
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
-# CREATING MESH
-#------------------------------------------------------------------------------
+    basename = base_dir + '/inputs/DEM/dem_existing_01'
+    outname = 'woll_ex_01'
+    meshname = base_dir + '/outputs/woll_ex_01.msh'
 
-#bounding_polygon = [[W, S], [E, S], [E, N], [W, N]]
-#interior_regions = anuga.read_polygon_dir(CatchmentDictionary, 'Model/Bdy')
-bounding_polygon = su.read_polygon('./inputs/Bdy/bdy_02.shp')
-create_mesh_from_regions(bounding_polygon,
-    boundary_tags={'south': [0], 'east': [1], 'north': [2], 'west': [3]},
-    maximum_triangle_area=50,
-    interior_regions=None,
-    filename=meshname,
-    use_cache=False, 
-    verbose=True)
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    # ENTER DOMAIN COORDINATES
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    # W=301300
+    # N=6186300
+    # E=302000
+    # S=6185700
 
-domain = Domain(meshname, use_cache=False, verbose=True)
-domain.set_name(outname)
-print domain.statistics()
+    #------------------------------------------------------------------------------
+    # CREATING MESH
+    #------------------------------------------------------------------------------
 
-poly_fun_pairs = [['Extent', basename + '.tif']]
-topography_function = qs.composite_quantity_setting_function(
-    poly_fun_pairs,
-    domain,
-    nan_treatment='exception',
-)
-domain.set_quantity('friction', 0.035)
-domain.set_quantity('stage', 0.0)
-domain.set_quantity('elevation', topography_function, verbose=True, alpha=0.99)
-domain.set_minimum_storable_height(0.005)
+    #bounding_polygon = [[W, S], [E, S], [E, N], [W, N]]
+    #interior_regions = anuga.read_polygon_dir(CatchmentDictionary, 'Model/Bdy')
+    bounding_polygon = su.read_polygon(base_dir + '/inputs/Bdy/bdy_02.shp')
+    #https://hydrata.com/geoserver/wfs?format_options=charset%3AUTF-8&typename=geonode%3Abdy_02&outputFormat=SHAPE-ZIP&version=1.0.0&service=WFS&request=GetFeature
+    create_mesh_from_regions(bounding_polygon,
+        boundary_tags={'south': [0], 'east': [1], 'north': [2], 'west': [3]},
+        maximum_triangle_area=50,
+        interior_regions=None,
+        filename=meshname,
+        use_cache=False,
+        verbose=True)
 
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-# APPLY RAINFALL
-#----------------------------------------------------------------------------------------------------------------------------------------------------
+    domain = Domain(meshname, use_cache=False, verbose=True)
+    domain.set_name(outname)
+    print domain.statistics()
 
-Rainfall_Gauge_directory = 'inputs/Rainfall/Gauge/'
-for filename in os.listdir(Rainfall_Gauge_directory):
-    Gaugefile = Rainfall_Gauge_directory+filename
-    Rainfile = 'inputs/Rainfall/rain/rain.tms'
-    polygon = anuga.read_polygon(Gaugefile)
-    rainfall = anuga.file_function(Rainfile, quantities='rate')
-    op1 = Polygonal_rate_operator(domain, rate=rainfall, factor=1.0e-3, polygon=polygon, default_rate = 0.0)
-               
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-# SETUP BOUNDARY CONDITIONS
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-    
-print 'Available boundary tags', domain.get_boundary_tags()
-    
-Br = anuga.Reflective_boundary(domain)
-Bd = anuga.Dirichlet_boundary([0.0,0.0,0.0])
+    poly_fun_pairs = [['Extent', basename + '.tif']]
+    topography_function = qs.composite_quantity_setting_function(
+        poly_fun_pairs,
+        domain,
+        nan_treatment='exception',
+    )
+    domain.set_quantity('friction', 0.035)
+    domain.set_quantity('stage', 0.0)
+    domain.set_quantity('elevation', topography_function, verbose=True, alpha=0.99)
+    domain.set_minimum_storable_height(0.005)
 
-domain.set_boundary({'interior': Br, 'exterior': Bd, 'west': Bd, 'south': Bd, 'north': Bd, 'east': Bd})
-  
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-# EVOLVE SYSTEM THROUGH TIME
-#----------------------------------------------------------------------------------------------------------------------------------------------------
-    
-t0 = time.time()
-    
-for t in domain.evolve(yieldstep = 60.0, finaltime = 3600.0):
-    domain.write_time()
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    # APPLY RAINFALL
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+
+    Rainfall_Gauge_directory = base_dir + '/inputs/Rainfall/Gauge/'
+    for filename in os.listdir(Rainfall_Gauge_directory):
+        Gaugefile = Rainfall_Gauge_directory+filename
+        Rainfile = base_dir + '/inputs/Rainfall/rain/rain.tms'
+        polygon = anuga.read_polygon(Gaugefile)
+        rainfall = anuga.file_function(Rainfile, quantities='rate')
+        op1 = Polygonal_rate_operator(domain, rate=rainfall, factor=1.0e-3, polygon=polygon, default_rate = 0.0)
+
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    # SETUP BOUNDARY CONDITIONS
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+
+    print 'Available boundary tags', domain.get_boundary_tags()
+
+    Br = anuga.Reflective_boundary(domain)
+    Bd = anuga.Dirichlet_boundary([0.0,0.0,0.0])
+
+    domain.set_boundary({'interior': Br, 'exterior': Bd, 'west': Bd, 'south': Bd, 'north': Bd, 'east': Bd})
+
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    # EVOLVE SYSTEM THROUGH TIME
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+
+    t0 = time.time()
+
+    for t in domain.evolve(yieldstep = 60.0, finaltime = 3600.0):
+        domain.write_time()
+
+
+    print "Done. Nice work."
+
+if __name__ == "__main__":
+    # TODO: parse argv for local development
+    run_woll_ex_01('1')
