998fd2dadf9027b4b8ac81d72b3daaa25b9231e1
Dave <davekennewell@gmail.com>
2017-11-03 21:55:49 +0400

    working version 5

--- woll_ex_01.py
+++ woll_ex_01.py
@@ -8,10 +8,10 @@ from anuga import file_function, Polygon_function, read_polygon, create_mesh_fro
 import anuga.utilities.quantity_setting_functions as qs
 import anuga.utilities.spatialInputUtil as su
 import getpass
-from ..database_utils import write_percentage_complete
+from ..database_utils import write_percentage_complete, query_database
 
 
-def start_woll_ex_01(run_id, Runs, session):
+def start_woll_ex_01(run_id, Runs, scenario_name, Scenario, session):
     print "start_woll_ex_01 has fired with run_id: %s" % run_id
     base_dir = os.getcwd() + '/tasks/woll_ex_01'
     print "base_dir is: %s" % base_dir
@@ -46,11 +46,19 @@ def start_woll_ex_01(run_id, Runs, session):
 
     #bounding_polygon = [[W, S], [E, S], [E, N], [W, N]]
     #interior_regions = anuga.read_polygon_dir(CatchmentDictionary, 'Model/Bdy')
-    bounding_polygon = su.read_polygon(base_dir + '/inputs/Bdy/bdy_02.shp')
+    bounding_polygon_filename = '%s/inputs/bounding_polygon/%s.shp' % (base_dir, query_database('bounding_polygon', run_id, Runs, session)[8:])
+    print 'bounding_polygon_filename: %s' % bounding_polygon_filename
+    bounding_polygon = su.read_polygon(bounding_polygon_filename)
+    try:
+        interior_holes = [].append(su.read_polygon(base_dir + '/inputs/interior_holes/interior_holes_01.shp'))
+    except AssertionError:
+        print 'warning: no interior_holes found.'
+        interior_holes = None
     create_mesh_from_regions(bounding_polygon,
         boundary_tags={'south': [0], 'east': [1], 'north': [2], 'west': [3]},
         maximum_triangle_area=50,
         interior_regions=None,
+        interior_holes=interior_holes,
         filename=meshname,
         use_cache=False,
         verbose=True)
@@ -69,6 +77,8 @@ def start_woll_ex_01(run_id, Runs, session):
     domain.set_quantity('friction', 0.035)
     domain.set_quantity('stage', 0.0)
     domain.set_quantity('elevation', topography_function, verbose=True, alpha=0.99)
+    # house_addition_function = qs.composite_quantity_setting_function(house_addition_poly_fun_pairs, domain)
+    # domain.add_quantity('elevation', house_addition_function, location='centroids')
     domain.set_minimum_storable_height(0.005)
 
     #----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -104,14 +114,22 @@ def start_woll_ex_01(run_id, Runs, session):
 
     yieldstep = 60.0
     finaltime = 3600.0
+
+    print type(run_id)
+    print type(Runs)
+    print type(scenario_name)
+    print type(Scenario)
+    print type(session)
+
+
     for t in domain.evolve(yieldstep, finaltime):
         domain.write_time()
         percentage_complete = round(domain.time/domain.finaltime, 3)*100
-        write_percentage_complete(run_id, Runs, session, percentage_complete)
+        write_percentage_complete(run_id, Runs, scenario_name, Scenario, session, percentage_complete)
 
     print "Done. Nice work."
 
 if __name__ == "__main__":
     # TODO: parse argv for local development
     run_id = 'local_'
-    start_woll_ex_01(run_id)
+    start_woll_ex_01(run_id, Runs='Runs', session='session')
